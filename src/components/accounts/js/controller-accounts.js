dee3b4af360807232581ad4f454061aaf9631b24
Erwan Beauvois <erwan@allmyt.eu>
2017-11-15 18:25:44 +0100

    Add CSS for accounts list

--- src/components/accounts/js/controller-accounts.js
+++ src/components/accounts/js/controller-accounts.js
@@ -14,7 +14,22 @@
         $scope.error = null;
 
         api.getMyAssets().then(function(assets) {
-          $scope.portfolios = apiUtils.portfolios(assets);
+          $scope.portfolios = apiUtils.portfolios(assets).map(function(portfolio) {
+            portfolio.accounts = portfolio.accounts.map(function(account) {
+              account.var24 = Math.random() - 0.5;
+              account.var168 = Math.random() - 0.5;
+              account.assets = [
+                { volume: 150, symbol: 'ETH' },
+                { volume: 3, symbol: 'BTC' },
+                { volume: 10, symbol: 'SAN' },
+                { volume: 2344578564, symbol: 'MCO' },
+                { volume: 13.4556666778, symbol: 'DASH' }
+              ];
+              account.balanceText = '123,456.78 $';
+              return account;
+            });
+            return portfolio;
+          });
           $scope.accounts = apiUtils.accounts(assets);
         }).catch(function(err) {
           $scope.error = err;

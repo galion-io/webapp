d8b3cc9e1d1b4e523470f108641125934a56265a
Erwan Beauvois <erwan@allmyt.eu>
2017-11-15 18:33:47 +0100

    Improve display on smartphone

--- src/components/menu/js/controller-menu.js
+++ src/components/menu/js/controller-menu.js
@@ -4,6 +4,8 @@
   window.angular.module('menu').controller('MenuCtrl', [
     '$scope',
     function($scope) {
-
+      $scope.hideMenu = function hideMenu() {
+        window.angular.element(window.document.body).removeClass('show-menu');
+      };
     }]);
 })(window);

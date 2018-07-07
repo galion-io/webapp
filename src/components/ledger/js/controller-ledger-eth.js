fc008742d9921923650beea671589c0afa29ef75
Erwan Beauvois <erwan@allmyt.eu>
2018-07-07 12:11:39 +0200

    Add support for network selection & additional addresses for Ledger tx

--- src/components/ledger/js/controller-ledger-eth.js
+++ src/components/ledger/js/controller-ledger-eth.js
@@ -2,17 +2,24 @@
 
 (function closure(window) {
   window.angular.module('ledger').controller('LedgerEthCtrl', [
+    '$q',
     '$window',
     '$scope',
     '$http',
     '$timeout',
     'sidepanel',
     'value',
-    function($window, $scope, $http, $timeout, sidepanel, value) {
+    function($q, $window, $scope, $http, $timeout, sidepanel, value) {
+      $scope.paths = [
+        { path: 'm/44\'/60\'/0\'', label: 'Ledger (ETH)' },
+        { path: 'm/44\'/60\'/0\'/0', label: 'Jaxx, Metamask, Exodus, Trezor (ETH), ...' },
+        { path: 'm/44\'/60\'/160720\'/0', label: 'Ledger (ETC)' },
+        { path: 'm/44\'/61\'/0\'/0', label: 'Trezor (ETC)' },
+        { path: 'm/44\'/1\'/0\'/0', label: 'Testnets' }
+      ];
       $scope.value = value;
       var ledger = $window.ledger;
       var ethereumjs = $window.ethereumjs;
-      var path = '44\'/60\'/0\'/0';
       $scope.data = {};
       $scope.err = null;
       $scope.compatible = null;
@@ -23,70 +30,99 @@
         ledger.comm_u2f.create_async(30).then(function(_comm) {
           comm = _comm;
           $scope.compatible = true;
-
-          tryReadEther();
         }).catch(function() {
           $scope.compatible = false;
         });
       };
       $scope.init();
 
-      function tryReadEther() {
-        $scope.err = null;
-        $scope.data = {};
-        new ledger.eth(comm).getAddress_async(path).then(function(data) {
-          $scope.data = {
-            advanced: false,
-            currency: 'eth',
-            address: data.address,
-            publicKey: data.publicKey,
-            gasPrice: '45',
-            gasLimit: '21000',
-            nonce: 0,
-            balance: 0
-          };
+      $scope.setPath = function setPath(pathItem) {
+        $scope.data.path = pathItem.path;
+        $scope.readAddresses(0, 10);
+      };
 
-          $scope.$apply();
+      $scope.readAddresses = function readAddresses(from, to) {
+        console.log('reading addresses.... ----------------------------');
+        var arr = [];
+        for (var i = from; i < to; i++) {
+          arr.push(i);
+        }
 
-          $http({
-            method: 'GET',
-            url: 'https://api.etherscan.io/api?module=account&action=txlist&sort=desc&address=' + data.address,
-            withCredentials: false
-          }).then(function(res) {
-            var lastNonce = 0;
-            res.data.result.forEach(function(tx) {
-              if (tx.from.toUpperCase() === data.address.toUpperCase() && Number(tx.nonce) > lastNonce) {
-                lastNonce = Number(tx.nonce);
-              }
+        $scope.data.addresses = [];
+        return $q.all(arr.map(function(index) {
+          console.log('reading', $scope.data.path + '/' + index)
+          return new ledger.eth(comm).getAddress_async($scope.data.path + '/' + index).then(function(data) {
+            return $http({
+              method: 'GET',
+              url: 'https://api.etherscan.io/api?module=account&action=balance&tag=latest&address=' + data.address,
+              withCredentials: false
+            }).then(function(res) {
+              var balance = Number(res.data.result) / 1000000000000000000; // wei -> eth
+              console.log(data.address, 'has balance of', balance);
+              $scope.data.addresses.push({
+                index: index,
+                address: data.address,
+                publicKey: data.publicKey,
+                balance: balance
+              });
             });
-            $scope.data.nonce = lastNonce + 1;
           });
-          $http({
-            method: 'GET',
-            url: 'https://api.etherscan.io/api?module=account&action=balance&tag=latest&address=' + data.address,
-            withCredentials: false
-          }).then(function(res) {
-            $scope.data.balance = Number(res.data.result) / 1000000000000000000; // wei -> eth
-          });
-          $http({
-            method: 'GET',
-            url: 'https://ethgasstation.info/json/ethgasAPI.json',
-            withCredentials: false
-          }).then(function(res) {
-            $scope.data.gasPrice = Math.ceil(res.data.safeLow / 10).toString();
-          });
-        }).catch(function() {
-          $timeout(tryReadEther, 1000);
+        })).catch(function() {
+          $timeout(function() {
+            $scope.readAddresses(from, to);
+          }, 1000);
         });
-      }
+      };
+
+      $scope.updateNonce = function updateNonce() {
+        return $http({
+          method: 'GET',
+          url: 'https://api.etherscan.io/api?module=account&action=txlist&sort=desc&address=' + $scope.data.tx.address,
+          withCredentials: false
+        }).then(function(res) {
+          var lastNonce = 0;
+          res.data.result.forEach(function(tx) {
+            if (tx.from.toUpperCase() === $scope.data.tx.address.toUpperCase() && Number(tx.nonce) > lastNonce) {
+              lastNonce = Number(tx.nonce);
+            }
+          });
+          $scope.data.tx.nonce = lastNonce + 1;
+        });
+      };
+
+      $scope.updateGasPrice = function updateGasPrice() {
+        return $http({
+          method: 'GET',
+          url: 'https://ethgasstation.info/json/ethgasAPI.json',
+          withCredentials: false
+        }).then(function(res) {
+          $scope.data.tx.gasPrice = Math.ceil(res.data.safeLow / 10).toString();
+        });
+      };
+
+      $scope.setAddress = function setAddress(add) {
+        $scope.data.tx = {
+          advanced: false,
+          currency: 'eth',
+          address: add.address,
+          publicKey: add.publicKey,
+          gasPrice: '45',
+          gasLimit: '21000',
+          nonce: 0,
+          balance: add.balance
+        };
+
+        $scope.updateNonce();
+        $scope.updateGasPrice();
+      };
 
       $scope.askSignEtherTransaction = function askSignEtherTransaction() {
         var raw = {
-          nonce: '0x' + Number($scope.data.nonce).toString(16),
-          gasPrice: '0x' + (Number($scope.data.gasPrice) * 1000000000).toString(16),
-          gasLimit: '0x' + Number($scope.data.gasLimit).toString(16),
-          to: $scope.data.to.toLowerCase(),
-          value: '0x' + (Number($scope.data.txValue) * 1000000000000000000).toString(16),
+          nonce: '0x' + Number($scope.data.tx.nonce).toString(16),
+          gasPrice: '0x' + (Number($scope.data.tx.gasPrice) * 1000000000).toString(16),
+          gasLimit: '0x' + Number($scope.data.tx.gasLimit).toString(16),
+          to: $scope.data.tx.to.toLowerCase(),
+          value: '0x' + (Number($scope.data.tx.txValue) * 1000000000000000000).toString(16),
           chainId: 1,
           r: '0x00',
           s: '0x00',
@@ -95,16 +131,16 @@
         };
         var txToSign = new ethereumjs.Tx(raw).serialize().toString('hex');
 
-        new ledger.eth(comm).signTransaction_async(path, txToSign).then(function(result) {
+        new ledger.eth(comm).signTransaction_async($scope.data.path, txToSign).then(function(result) {
           raw.r = '0x' + result.r;
           raw.s = '0x' + result.s;
           raw.v = '0x' + result.v;
           var txSigned = new ethereumjs.Tx(raw);
           txSigned._chainId = raw.chainId;
-          txSigned._senderPubKey = $scope.data.publicKey;
+          txSigned._senderPubKey = $scope.data.tx.publicKey;
 
-          $scope.data.txRaw = raw;
-          $scope.data.txSigned = '0x' + txSigned.serialize().toString('hex');
+          $scope.data.tx.txRaw = raw;
+          $scope.data.tx.txSigned = '0x' + txSigned.serialize().toString('hex');
 
           $scope.$apply();
         }).catch(function() {});

86c35a62208d71af6ad95ec3e1781d9a26ff2149
Erwan Beauvois <erwan@allmyt.eu>
2018-08-22 23:10:12 +0200

    Remove old onboarding

--- src/components/onboarding/module.js
+++ src/components/onboarding/module.js
@@ -1,22 +1,8 @@
 'use strict';
 
 (function closure(window) {
-  var module = window.angular.module('onboarding', [
-    'ui.router',
+  window.angular.module('onboarding', [
     'templates',
-    'pascalprecht.translate',
-    'ui.select',
-    'api'
-  ]);
-
-  module.config([
-    '$stateProvider',
-    function config($stateProvider) {
-      $stateProvider.state('onboarding', {
-        url: '/onboarding',
-        controller: 'OnboardingCtrl',
-        templateUrl: 'onboarding/templates/onboarding.html'
-      });
-    }
+    'pascalprecht.translate'
   ]);
 })(window);

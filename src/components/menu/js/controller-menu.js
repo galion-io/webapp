531376d9c4f44f455c9f25a7a90b5a148f83a7a6
Erwan Beauvois <erwan@allmyt.eu>
2018-08-24 19:24:24 +0200

    Add onboarding checklist

--- src/components/menu/js/controller-menu.js
+++ src/components/menu/js/controller-menu.js
@@ -4,7 +4,9 @@
   window.angular.module('menu').controller('MenuCtrl', [
     '$scope',
     'sidepanel',
-    function($scope, sidepanel) {
+    'api',
+    'apiUtils',
+    function($scope, sidepanel, api, apiUtils) {
       $scope.hideMenu = function hideMenu() {
         window.angular.element(window.document.body).removeClass('show-menu');
       };
@@ -12,5 +14,20 @@
       $scope.promptLedgerEth = function() {
         sidepanel.show('ledger/templates/sidepanel-eth.html');
       };
+
+      $scope.onboardingPercentage = function onboardingPercentage() {
+        return (
+          ($scope.user.email_verified ? 1 : 0) +
+          ($scope.portfolios && $scope.portfolios.length ? 1 : 0) +
+          ($scope.accounts && $scope.accounts.length ? 1 : 0) +
+          ($scope.assets && $scope.assets.length ? 1 : 0)
+        ) / 4;
+      };
+
+      api.getMyAssets().then(function(myAssets) {
+        $scope.portfolios = apiUtils.portfolios(myAssets);
+        $scope.accounts = apiUtils.accounts(myAssets);
+        $scope.assets = apiUtils.allAssets(myAssets);
+      });
     }]);
 })(window);
